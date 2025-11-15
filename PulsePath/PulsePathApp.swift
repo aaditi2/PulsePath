3eecc99183b275a637287bd960b2eb17c0739afc
Aditi More <adit.more2001@gmail.com>
2025-11-14 17:48:02 -0800

    structure

---
+++ PulsePath/PulsePathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsePathApp.swift
+//  PulsePath
+//
+//  Created by Aditi More on 11/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PulsePathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
